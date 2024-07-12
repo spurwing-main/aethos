395c80c3e65b8880a26afdc33c9f520cf18c75fe
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2024-07-12 11:34:13 +0100

    Add updateCopyrightYear()

--- aethos.js
+++ aethos.js
@@ -1,1 +1,7 @@
 /* sitewide custom code goes here */
+function updateCopyrightYear() {
+	const year = new Date().getFullYear().toString();
+	document
+		.querySelectorAll('[copyright="year"]')
+		.forEach((el) => (el.textContent = year));
+}
