a8f98a40e53a17b11a35299206615955e2e2004a
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2024-11-13 12:33:54 +0000

    Open external links in new tab

--- aethos.js
+++ aethos.js
@@ -183,6 +183,23 @@ function main() {
 	// Execute the function
 	aethos.functions.updateRelativeLinks();
 
+	// Open all external links in a new tab
+	(function openExternalLinksInNewTab() {
+		const links = document.querySelectorAll(
+			"a[href^='https://'], a[href^='http://']"
+		);
+		const host = window.location.hostname;
+
+		const isInternalLink = (link) => new URL(link).hostname === host;
+
+		links.forEach((link) => {
+			if (isInternalLink(link)) return;
+
+			link.setAttribute("target", "_blank");
+			link.setAttribute("rel", "noopener");
+		});
+	})();
+
 	/* helper to get a custom prop value */
 	aethos.helpers.getProp = function (
 		propName,
