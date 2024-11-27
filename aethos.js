485ab1e3b56d8cc77c92dd0a507d978b7492ed2e
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2024-11-27 12:27:29 +0000

    patches for updating nav state on resize

--- aethos.js
+++ aethos.js
@@ -354,22 +354,52 @@ function main() {
 		});
 	};
 
-	/* add class to <body> when .nav is open. Used for animating nav burger icon, potentially for handling other elements down the line */
+	aethos.helpers.enableScroll = function () {
+		var enable = document.querySelector(".fs-enable-scroll");
+		if (enable) {
+			enable.click();
+		}
+	};
+
+	aethos.helpers.disableScroll = function () {
+		var disable = document.querySelector(".fs-disable-scroll");
+		if (disable) {
+			disable.click();
+		}
+	};
+
+	/* add class to <body> when .nav is open. Used for animating nav burger icon */
 	aethos.functions.nav = function () {
 		aethos.helpers.globalNavClass = "nav-open";
 		aethos.helpers.destNavClass = "dest-nav-open";
 		aethos.helpers.clubNavClass = "club-nav-open";
 
 		function handleResize() {
-			// Close the menu if the window width drops below the breakpoint
-			if (window.innerWidth <= aethos.breakpoints.mbl) {
-				document.body.classList.remove(aethos.helpers.globalNavClass);
+			// default - remove global nav class on mbl and smaller on close
+			if (aethos.settings.theme == "default" || !aethos.settings.theme) {
+				if (window.innerWidth <= aethos.breakpoints.mbl) {
+					document.body.classList.remove(aethos.helpers.globalNavClass);
+					aethos.helpers.enableScroll();
+				}
 			}
-			if (window.innerWidth <= aethos.breakpoints.tab) {
-				document.body.classList.remove(aethos.helpers.destNavClass);
+			// dest - remove dest nav class on tab and smaller on close
+			if (
+				aethos.settings.theme &&
+				aethos.settings.theme !== "default" &&
+				aethos.settings.theme !== "club"
+			) {
+				if (window.innerWidth <= aethos.breakpoints.tab) {
+					document.body.classList.remove(aethos.helpers.destNavClass);
+					document.body.classList.remove(aethos.helpers.globalNavClass);
+					aethos.helpers.enableScroll();
+				}
 			}
-			if (window.innerWidth <= aethos.breakpoints.tab) {
-				document.body.classList.remove(aethos.helpers.clubNavClass);
+			if (aethos.settings.theme == "club") {
+				if (window.innerWidth <= aethos.breakpoints.tab) {
+					document.body.classList.remove(aethos.helpers.clubNavClass);
+					document.body.classList.remove(aethos.helpers.globalNavClass);
+					aethos.helpers.enableScroll();
+				}
 			}
 		}
 
@@ -425,348 +455,6 @@ function main() {
 			});
 		}
 	};
-
-	/* nav logo height anim INCOMPLETE */
-	aethos.anim.navHeight = function () {
-		const header = document.querySelector(".header");
-		const logo = header.querySelector(".header-bar_middle");
-		const headerHeight_large = aethos.helpers.getProp("--c--header--h", header);
-
-		// Only run this function if the pageLoader setting is true
-		if (!aethos.settings.pageLoader) {
-			return;
-		}
-
-		ScrollTrigger.create({
-			start: "top " + headerHeight_large,
-			end: 99999,
-			toggleClass: { className: "scrolled", targets: ".site-header" },
-		});
-
-		return;
-
-		// aethos.log("Page loader is enabled, running navHeight animation.");
-
-		// Store the initial and smaller navbar heights (CSS variables are used for flexibility)
-		// const headerHeight_large = aethos.helpers.getProp("--c--header--h");
-		const logoHeight_lg = aethos.helpers.getProp(
-			"--c--header--logo-h-lg",
-			header
-		);
-		const logoHeight_sm = aethos.helpers.getProp(
-			"--c--header--logo-h-sm",
-			header
-		);
-		// console.log(logoHeight_lg);
-		// const smallerHeight = "calc(0.8 * var(--c--header--h))"; // Adjust this multiplier for the reduced height
-
-		// const navHeight = gsap
-		// 	.fromTo(
-		// 		logo,
-		// 		{ height: logoHeight_sm },
-		// 		{ height: logoHeight_lg, duration: 0.8, ease: "power2.out" }
-		// 	)
-		// 	.progress(1);
-
-		// Set up GSAP ScrollTrigger to animate the navbar height
-		ScrollTrigger.create({
-			trigger: header, // Start reducing the height after scrolling down 10px
-			start: "top top+=20",
-			markers: true,
-			// end: "max", // Keep the effect throughout the scroll
-			// onEnter: () => navHeight.reverse(),
-			// onEnterBack: () => navHeight.play(),
-			onEnter: function () {
-				gsap.to(logo, {
-					height: logoHeight_sm,
-					duration: 0.8,
-					ease: "power2.out",
-				});
-			},
-			onEnterBack: function () {
-				gsap.to(logo, {
-					height: logoHeight_sm,
-					duration: 0.8,
-					ease: "power2.out",
-				});
-			},
-
-			// onUpdate: (self) => {
-			// 	if (self.direction === 1 && window.scrollY > 10) {
-			// 		// Scrolling down: reduce height
-			// 		gsap.to(logo, {
-			// 			height: logoHeight_sm,
-			// 			duration: 0.3,
-			// 			ease: "power2.out",
-			// 		});
-			// 	} else if (self.direction === -1 && window.scrollY <= 10) {
-			// 		// Scrolling up: restore initial height
-			// 		gsap.to(logo, {
-			// 			height: logoHeight_lg,
-			// 			duration: 0.3,
-			// 			ease: "power2.out",
-			// 		});
-			// 	}
-			// },
-		});
-	};
-
-	aethos.anim.loader = function () {
-		return;
-		// Only run this function if the pageLoader setting is true and the loader hasn't run before
-		if (
-			!aethos.settings.pageLoader ||
-			localStorage.getItem("aethos_loader_ran")
-		) {
-			return;
-		}
-
-		// Mark the loader as "played" in local storage, expiring in 30 days
-		const expirationDate = new Date();
-		expirationDate.setDate(expirationDate.getDate() + 30);
-		localStorage.setItem("aethos_loader_ran", true);
-		localStorage.setItem("aethos_loader_expiration", expirationDate.getTime());
-
-		// Get elements
-		let header = document.querySelector(".header");
-		let loader = document.querySelector(".site-loader");
-		// let gsap_loader = gsap.utils.selector(loader);
-		// let gsap_header = gsap.utils.selector(header);
-
-		// Get Lottie
-		let loader_lottie = lottie.loadAnimation({
-			container: document.querySelector(".site-loader_lottie"),
-			renderer: "svg",
-			loop: false,
-			autoplay: false,
-			path: "https://cdn.prod.website-files.com/668fecec73afd3045d3dc567/66d03670c7268e6c895d7847_Aethos%20Logo%20Lottie%20v2.json",
-		});
-
-		let playhead = { frame: 0 };
-
-		// Set initial states
-		gsap.set(loader, { display: "flex", autoAlpha: 1 });
-		gsap.set(".site-loader_overlay-bottom", { backgroundColor: "black" });
-		gsap.set(".site-loader_lottie", { autoAlpha: 1, display: "block" });
-		gsap.set(".site-loader_logo", { height: "auto", autoAlpha: 0 });
-		gsap.set(".site-loader_img-container", { autoAlpha: 0 });
-		gsap.set(".site-loader_img", { scale: 0.75 });
-		gsap.set([".header-bar_left", ".header-bar_right"], { y: "200%" });
-		gsap.set(".site-loader_overlay-top", { backgroundColor: "black" });
-		gsap.set(".header-bar_middle", { autoAlpha: 0 });
-		gsap.set(".site-loader_img-wrap", { width: "50%", height: "75%" });
-		gsap.set(".hero-home_content", { autoAlpha: 0, y: "20%" });
-
-		// Create timeline
-		let tl = gsap.timeline({ paused: true });
-
-		// Play Lottie animation [5s]
-		tl.to(playhead, {
-			frame: loader_lottie.totalFrames - 1,
-			duration: 5,
-			ease: "none",
-			onUpdate: () => loader_lottie.goToAndStop(playhead.frame, true),
-		});
-
-		// Logo height animation, occurs simultaneously with Lottie [5.3s]
-		tl.to(".site-loader_logo", { height: "100%", duration: 5 }, 0.3);
-
-		// At 5.3 seconds: lottie fades out, logo fades in, overlay colors change
-		tl.to(
-			".site-loader_lottie",
-			{ autoAlpha: 0, duration: 1.25, ease: "power4.inOut" },
-			5.3
-		);
-		tl.to(
-			".site-loader_logo",
-			{ autoAlpha: 1, duration: 1.25, ease: "power4.inOut" },
-			5.3
-		);
-		tl.to(
-			".site-loader_overlay-bottom",
-			{ backgroundColor: "black", duration: 1.25, ease: "power4.inOut" },
-			5.3
-		); // Change to the correct page-bg color later
-		tl.to(
-			".site-loader_overlay-top",
-			{ backgroundColor: "transparent", duration: 1.25, ease: "power4.inOut" },
-			5.3
-		);
-
-		// At 5.55s: Show image container [1s ease in]
-		tl.to(
-			".site-loader_img-container",
-			{ autoAlpha: 1, duration: 1, ease: "power4.in" },
-			5.55
-		);
-
-		// At 6.55s: Scale image to 1, adjust logo, move header bars, adjust image wrap [various durations]
-		tl.to(
-			".site-loader_img",
-			{ scale: 1, duration: 1.5, ease: "power4.inOut" },
-			6.55
-		);
-		tl.to(
-			".site-loader_logo",
-			{ height: "auto", duration: 1.5, ease: "power4.inOut" },
-			6.55
-		);
-		tl.to(
-			".header-bar_left",
-			{ y: "0%", duration: 0.4, ease: "power4.inOut" },
-			6.55
-		);
-		tl.to(
-			".header-bar_right",
-			{ y: "0%", duration: 0.4, ease: "power4.inOut" },
-			6.55
-		);
-		tl.to(
-			".site-loader_img-wrap",
-			{ width: "100%", height: "100%", duration: 0.5, ease: "linear" },
-			6.55
-		);
-
-		// At 8.8s: Show hero-home content [0.4s ioq]
-		tl.to(
-			".hero-home_content",
-			{ autoAlpha: 1, y: "0%", duration: 0.4, ease: "power4.inOut" },
-			8.8
-		);
-
-		// At 9.2s: Show header middle bar and hide loader
-		tl.to(
-			".header-bar_middle",
-			{ autoAlpha: 1, display: "flex", duration: 0 },
-			9.2
-		);
-		tl.to(loader, { autoAlpha: 0, display: "none", duration: 0 }, 9.2);
-
-		// Play the timeline
-		tl.play();
-		aethos.log("loader play");
-		tl.pause(4.8);
-	};
-
-	// Check and remove expired local storage entry for loader animation
-	aethos.helpers.clearExpiredLoader = function () {
-		const expiration = localStorage.getItem("aethos_loader_expiration");
-		if (expiration && new Date().getTime() > expiration) {
-			localStorage.removeItem("aethos_loader_ran");
-			localStorage.removeItem("aethos_loader_expiration");
-		}
-	};
-
-	// Call clearExpiredLoader when the page loads
-	aethos.helpers.clearExpiredLoader();
-
-	// /* homepage loader */
-	// aethos.anim.loader_v1 = function () {
-	// 	// Only run this function if the pageLoader setting is true
-	// 	if (!aethos.settings.pageLoader) {
-	// 		return;
-	// 	}
-
-	// 	/* get els */
-	// 	let header = document.querySelector(".header");
-	// 	let loader = document.querySelector(".site-loader");
-	// 	let gsap_loader = gsap.utils.selector(loader);
-	// 	let gsap_header = gsap.utils.selector(header);
-
-	// 	/* get lottie */
-	// 	let loader_lottie = lottie.loadAnimation({
-	// 		container: loader.querySelector(".site-loader_lottie"),
-	// 		renderer: "svg",
-	// 		loop: false,
-	// 		autoplay: false,
-	// 		path: "https://cdn.prod.website-files.com/668fecec73afd3045d3dc567/66d03670c7268e6c895d7847_Aethos%20Logo%20Lottie%20v2.json",
-	// 	});
-	// 	//   loader_lottie.setSpeed(5);
-	// 	let playhead = { frame: 0 };
-
-	// 	/* set initial states */
-	// 	gsap.set(loader, { display: flex });
-	// 	gsap.set(".site-loader_overlay-bottom", { backgroundColor: "black" });
-	// 	gsap.set(".site-loader_lottie", { autoAlpha: 1 });
-	// 	gsap.set(loader, { autoAlpha: 1 });
-	// 	gsap.set(".site-loader_logo", { height: auto, autoAlpha: 0 });
-	// 	gsap.set(".site-loader_img-container", { autoAlpha: 0 });
-	// 	gsap.set(".site-loader_img", { scale: 0.75 });
-	// 	gsap.set([".header-bar_left", ".header-bar_right"], { y: "200%" });
-	// 	gsap.set(".site-loader_overlay-top", { backgroundColor: "black" });
-	// 	gsap.set(".header-bar_middle", { autoAlpha: 0 });
-	// 	gsap.set(".site-loader_img-wrap ", { width: "50%", height: "75%" });
-	// 	gsap.set(".hero-home_content", { autoAlpha: 0, y: "20%" });
-
-	// 	/* create timeline */
-	// 	let tl = gsap.timeline({
-	// 		paused: true,
-	// 	});
-
-	// 	tl.to(playhead, {
-	// 		frame: loader_lottie.totalFrames - 1,
-	// 		duration: 5,
-	// 		ease: "none",
-	// 		onUpdate: () => loader_lottie.goToAndStop(playhead.frame, true),
-	// 	});
-
-	// 	/*
-
-	// 	Start:
-
-	// 	show loader (flex)
-	// 	overlay color .site-loader_overlay-bottom
-	// 	start lottie at zero
-	// 	lottie 100% opacity
-	// 	loader 100% opacity
-	// 	.site-loader_logo size height auto, hidden, opacity 0
-	// 	.site-loader_img-container hidden, opacity 0
-	// 	<clips> TBC
-	// 	.site-loader_img scale 0.75
-	// 	.header-bar_left, .header-bar_right Y -200%
-	// 	.site-loader_overlay-top BG color body
-	// 	.header-bar_middle hidden
-	// 	.site-loader_img-wrap size W 50% H 75%
-	// 	.hero-home_content opacity 0, Y 20px
-
-	// 	0.3s
-
-	// 	lottie play [5s]
-	// 	.site-loader_logo size height 100%
-
-	// 	5.3s
-
-	// 	_logo display flex
-	// 	_img-container display flex
-	// 	lottie opacity 0 [1.25s inoutquart]
-	// 	_logo opacity 1 [1.25s inoutquart]
-	// 	_overlay-bottom bg color page-bg [1.25s inoutquart]
-	// 	_overaly-top bg color transparent [1.25s inoutquart]
-
-	// 	5.55s
-
-	// 	_img-container opacity 1 [1s ease in]
-
-	// 	6.55s
-
-	// 	<clips> TBC
-	// 	_img scale 1 [1.5s ioq]
-	// 	_logo height auto [1.5s ioq]
-	// 	_left move y 0 [0.4 ioq]
-	// 	_right move y 0 [0.4 ioq]
-	// 	_img-wrap size 100% 100% [0.5 linear]
-
-	// 	8.8s
-
-	// 	hero-home_content opacity 1, move y 0 [0.4 ioq]
-
-	// 	9.2s
-
-	// 	header-bar_middle display flex
-	// 	site-loader hide
-
-	// 	*/
-	// };
 
 	/* Basic slide and fade animation */
 	aethos.anim.fadeUp = function () {
@@ -3082,6 +2770,7 @@ function main() {
 		});
 	};
 
+	/* FAQ */
 	aethos.anim.faq = function () {
 		// get all sections on page
 		let sections = document.querySelectorAll(".c-faq");
@@ -3207,6 +2896,7 @@ function main() {
 		});
 	};
 
+	/* benefits */
 	aethos.anim.benefits = function () {
 		// Set height: 0 on page load
 		gsap.set(".mem-benefits_list-wrap", { height: 0 });
@@ -3271,6 +2961,7 @@ function main() {
 		});
 	};
 
+	/* calculator */
 	aethos.functions.calc = function () {
 		const form = document.querySelector(".calc");
 		if (!form) {
@@ -3452,6 +3143,7 @@ function main() {
 		resetForm();
 	};
 
+	/* underline effect on hover for club nav */
 	aethos.anim.addClubNavHover = function () {
 		const menus = document.querySelectorAll(".club-nav");
 
@@ -3471,10 +3163,6 @@ function main() {
 						// Calculate the offset relative to the menu
 						const offsetX = targetRect.left - menuRect.left;
 
-						// console.log(event.target);
-						// console.log(menu);
-						// console.log(menuRect);
-						// console.log(targetRect);
 						console.log(offsetX);
 
 						// Set underline width and position properties
@@ -3505,6 +3193,7 @@ function main() {
 		});
 	};
 
+	/* underline effect on hover for tabs */
 	aethos.anim.wellTabsUnderline = function () {
 		const tabMenu = document.querySelector(".well-tabs_menu");
 		if (!tabMenu) {
@@ -3568,6 +3257,7 @@ function main() {
 		window.addEventListener("resize", updateUnderline);
 	};
 
+	/* club nav */
 	aethos.functions.clubNav = function () {
 		// open Global Nav from Club Nav
 		const globalNavLink = document.querySelector(".club-nav_item.is-global");
@@ -3575,8 +3265,8 @@ function main() {
 		const clubMenuButton = document.querySelector(".header .club-nav-btn");
 		if (globalNavLink && globalMenuButton && clubMenuButton) {
 			globalNavLink.addEventListener("click", (event) => {
-				clubMenuButton.click();
-				globalMenuButton.click();
+				clubMenuButton.click(); // close club nav
+				globalMenuButton.click(); // open global nav
 			});
 		} else {
 		}
@@ -3590,18 +3280,12 @@ function main() {
 	};
 
 	/* CALL FUNCTIONS */
-	// aethos.anim.pageTransition();
-
 	aethos.functions.nav();
 	aethos.functions.buildDestinationNav();
-
 	aethos.functions.hiddenFormFields();
-	// aethos.functions.clearSelect();
-
 	aethos.functions.handleCMSLoad();
 	aethos.functions.handleCMSFilter();
 	aethos.functions.hideEmptySections();
-
 	aethos.anim.splitText();
 	aethos.anim.splitTextBasic();
 	aethos.anim.fadeUp();
@@ -3626,32 +3310,17 @@ function main() {
 	aethos.anim.clubMemberships();
 	aethos.anim.faq();
 	aethos.anim.benefits();
-
 	aethos.functions.listingLinks();
-
 	aethos.functions.updateSubscribeFormName();
-
-	aethos.anim.loader();
-
 	aethos.functions.loadVideos();
-
 	aethos.anim.carousel();
-
 	aethos.functions.patches();
-
 	aethos.functions.retreatOutline();
-
 	aethos.functions.calc();
-
 	aethos.anim.addClubNavHover();
-
 	aethos.functions.langSwitcherClose();
-
 	aethos.anim.mblLangSlider();
-
 	aethos.functions.formSubmissionStyling();
-
 	aethos.anim.wellTabsUnderline();
-
 	aethos.functions.clubNav();
 }
